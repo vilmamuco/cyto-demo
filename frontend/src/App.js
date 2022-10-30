72d1f5a3a685c9975677bc9b627569a33f8c156e
Vilma Muço <muco.vilma@gmail.com>
2022-10-30 07:15:18 +0100

    intial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,43 @@
+import logo from "./logo.svg";
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch("/time")
+      .then(
+        (res) => res.json(),
+        (reason) => {
+          console.log("error:", reason);
+          return {time: "FUCK"};
+        }
+      )
+      .then((data) => {
+        console.log(data);
+        setCurrentTime(data.time);
+      });
+  }, []);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Coucou! <code>src/App.js</code> and save to reload.
+        </p>
+        <p>The current time is {currentTime} .</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
