cd6fce03d6babbf26607eed9f230dba8df081f22
Vilma Muço <muco.vilma@gmail.com>
2022-11-06 13:54:45 +0100

    format with prettier

--- frontend/src/graphComponent.jsx
+++ frontend/src/graphComponent.jsx
@@ -1,241 +1,307 @@
-import React, { Component } from 'react';
-import CytoscapeComponent from 'react-cytoscapejs';
-import contextMenus from 'cytoscape-context-menus';
-import 'cytoscape-context-menus/cytoscape-context-menus.css';
-import cytoscape from 'cytoscape';
-import { v4 as uuidv4 } from 'uuid';
+import React, { Component } from "react";
+import CytoscapeComponent from "react-cytoscapejs";
+import contextMenus from "cytoscape-context-menus";
+import "cytoscape-context-menus/cytoscape-context-menus.css";
+import cytoscape from "cytoscape";
+import { v4 as uuidv4 } from "uuid";
 
 // icons taken from https://uxwing.com/
 
 cytoscape.use(contextMenus);
 export default class Graph extends Component {
-  
   constructor(props) {
     super(props);
     this.state = {
-      selectedNodeId: ''
-      };
+      selectedNodeId: "",
+    };
   }
 
   render() {
-
     const layout = {
-        name: 'cose'
+      name: "cose",
     };
-    
+
     const cytoscapeStylesheet = [
       {
-        selector: 'node',
+        selector: "node",
         style: {
-          'label': 'data(label)' // here you can label the nodes 
-        }
-      },      
+          label: "data(label)", // here you can label the nodes
+        },
+      },
       {
         selector: "node[label]",
         style: {
-          color: "white"
-        }
+          color: "white",
+        },
       },
       {
         selector: "edge",
         style: {
           "curve-style": "bezier",
           "target-arrow-shape": "triangle",
-          width: 1.5
-        }
-      }
+          width: 1.5,
+        },
+      },
     ];
 
-    return(
-        <CytoscapeComponent
-            elements={this.props.elements}
-            stylesheet={cytoscapeStylesheet}
-            style={{
-                width: "800px",
-                height: "500px",
-                border: "1px solid black",
-            }}
-            cy={(cy) => {
-              this.cy = cy
-            }}
-            layout={layout}
-        />
+    return (
+      <CytoscapeComponent
+        elements={this.props.elements}
+        stylesheet={cytoscapeStylesheet}
+        style={{
+          width: "800px",
+          height: "500px",
+          border: "1px solid black",
+        }}
+        cy={(cy) => {
+          this.cy = cy;
+        }}
+        layout={layout}
+      />
     );
   }
- 
-  componentDidMount() {
-    this.cy.layout({
-      name: 'cose'
-    }).run();
 
-    this.cy.on('dragfree', 'node', (e) => {
-      //save position of node on database after dragging 
+  componentDidMount() {
+    this.cy
+      .layout({
+        name: "cose",
+      })
+      .run();
+
+    this.cy.on("dragfree", "node", (e) => {
+      //save position of node on database after dragging
       var node = e.target;
       this.props.onPositionChange(node);
       console.warn(node.position());
     });
 
-    this.cy.on('click', 'node', (e) => {
-      if (this.state.selectedNodeId != ''){
-        //add a new edge 
-        var data= {"target": e.target.data('id'), "source": this.state.selectedNodeId};
+    this.cy.on("click", "node", (e) => {
+      if (this.state.selectedNodeId != "") {
+        //add a new edge
+        var data = {
+          target: e.target.data("id"),
+          source: this.state.selectedNodeId,
+        };
         this.props.addNewEdge(data);
-        var targetnode = this.cy.$('#'+ this.state.selectedNodeId);
-        if (targetnode.edgesTo( e.target ).length == 0){
+        var targetnode = this.cy.$("#" + this.state.selectedNodeId);
+        if (targetnode.edgesTo(e.target).length == 0) {
           e.cy.add({
-            'data': data
+            data: data,
           });
         }
         this.setState({
-          selectedNodeId: ''
+          selectedNodeId: "",
         });
-      }else{
+      } else {
         //click on node to modify
-        var data= {"id": e.target.data('id'), "label": e.target.data('label'), "backgroundColor": e.target.style('background-color')};
+        var data = {
+          id: e.target.data("id"),
+          label: e.target.data("label"),
+          backgroundColor: e.target.style("background-color"),
+        };
         this.props.modifyNodeForm(data);
       }
     });
 
-     var contextMenu = this.cy.contextMenus({
+    var contextMenu = this.cy.contextMenus({
       menuItems: [
         {
-          id: 'delete',
-          content: ' Delete',
-          tooltipText: 'delete',
-          image: {src: require("./images/danger-icon.svg").default, width: 20, height: 20, x: 0, y: 4},
-          selector: 'node',
+          id: "delete",
+          content: " Delete",
+          tooltipText: "delete",
+          image: {
+            src: require("./images/danger-icon.svg").default,
+            width: 20,
+            height: 20,
+            x: 0,
+            y: 4,
+          },
+          selector: "node",
           onClickFunction: (event) => {
             var target = event.target || event.cyTarget;
             target.remove();
-            this.props.deleteNodeFunct(target.data('id'));
+            this.props.deleteNodeFunct(target.data("id"));
           },
-          hasTrailingDivider: true
+          hasTrailingDivider: true,
         },
         {
-          id: 'deleteEdge',
-          content: ' Delete',
-          tooltipText: 'delete',
-          image: {src: require("./images/danger-icon.svg").default, width: 20, height: 20, x: 0, y: 4},
-          selector: 'edge',
+          id: "deleteEdge",
+          content: " Delete",
+          tooltipText: "delete",
+          image: {
+            src: require("./images/danger-icon.svg").default,
+            width: 20,
+            height: 20,
+            x: 0,
+            y: 4,
+          },
+          selector: "edge",
           onClickFunction: (event) => {
             var target = event.target || event.cyTarget;
             target.remove();
-            var dataInput= {"source": target.data('source'), "target": target.data('target')};
+            var dataInput = {
+              source: target.data("source"),
+              target: target.data("target"),
+            };
             this.props.deleteEdge(dataInput);
           },
-          hasTrailingDivider: true
+          hasTrailingDivider: true,
         },
-        {id: 'addNodeEdge',
-          content: ' Click to select a target node',
-          tooltipText: 'Click a node to select it as a target node',
-          image: {src: require("./images/cluster-data-icon.svg").default, width: 20, height: 20, x: 0, y: 4},
-          selector: 'node',
+        {
+          id: "addNodeEdge",
+          content: " Click to select a target node",
+          tooltipText: "Click a node to select it as a target node",
+          image: {
+            src: require("./images/cluster-data-icon.svg").default,
+            width: 20,
+            height: 20,
+            x: 0,
+            y: 4,
+          },
+          selector: "node",
           onClickFunction: (event) => {
             var target = event.target || event.cyTarget;
             this.setState({
-              selectedNodeId: target.data('id')
+              selectedNodeId: target.data("id"),
             });
-          }
+          },
         },
         {
-          id: 'addNode',
-          content: ' Add node',
-          tooltipText: 'Add node',
-          image: {src: require('./images/math-plus-icon.svg').default, width: 20, height: 20, x: 0, y: 4},
-          selector: 'node, edge',
+          id: "addNode",
+          content: " Add node",
+          tooltipText: "Add node",
+          image: {
+            src: require("./images/math-plus-icon.svg").default,
+            width: 20,
+            height: 20,
+            x: 0,
+            y: 4,
+          },
+          selector: "node, edge",
           coreAsWell: true,
           onClickFunction: (event) => {
-            var nodeId=uuidv4();
-            var nodeLabel = 'Label'
+            var nodeId = uuidv4();
+            var nodeLabel = "Label";
             var pos = event.position || event.cyPosition;
             event.cy.add({
               data: {
                 id: nodeId,
-                label: nodeLabel
+                label: nodeLabel,
               },
               position: {
                 x: pos.x,
-                y: pos.y
+                y: pos.y,
               },
-              style:{'background-color':'#ff79c6', 'color':"white"}
+              style: { "background-color": "#ff79c6", color: "white" },
             });
-            var dataInput= {"id": nodeId, "label": nodeLabel, "x": pos.x, 'y': pos.y, 'backgroundColor':'#ff79c6'};
+            var dataInput = {
+              id: nodeId,
+              label: nodeLabel,
+              x: pos.x,
+              y: pos.y,
+              backgroundColor: "#ff79c6",
+            };
             this.props.saveNewElement(dataInput);
-          }
+          },
         },
         // add color submenu
         // adapted from https://github.com/iVis-at-Bilkent/cytoscape.js-context-menus/blob/97877acfa77914ee01c4c74c12b0e1ccc362852d/demo.html
         {
-          id: 'color',
-          content: ' Change color',
-          tooltipText: 'Change color',
-          selector: 'node',
-          image: {src: require("./images/paint-brush-drawing-icon.svg").default, width: 20, height: 20, x: 0, y: 4},
+          id: "color",
+          content: " Change color",
+          tooltipText: "Change color",
+          selector: "node",
+          image: {
+            src: require("./images/paint-brush-drawing-icon.svg").default,
+            width: 20,
+            height: 20,
+            x: 0,
+            y: 4,
+          },
           hasTrailingDivider: true,
           submenu: [
             {
-              id: 'color-blue',
-              content: 'blue',
-              tooltipText: 'blue',
+              id: "color-blue",
+              content: "blue",
+              tooltipText: "blue",
               onClickFunction: (e) => {
                 let target = e.target || e.cyTarget;
-                target.style('background-color', 'blue');
-                var data = {"id": target.data('id'), 'backgroundColor': target.style('background-color')};
+                target.style("background-color", "blue");
+                var data = {
+                  id: target.data("id"),
+                  backgroundColor: target.style("background-color"),
+                };
                 this.props.updateNodeColor(data);
               },
               submenu: [
                 {
-                  id: 'color-light-blue',
-                  content: 'light blue',
-                  tooltipText: 'light blue',
+                  id: "color-light-blue",
+                  content: "light blue",
+                  tooltipText: "light blue",
                   onClickFunction: (event) => {
                     let target = event.target || event.cyTarget;
-                    target.style('background-color', 'lightblue');
-                    var data = {"id": target.data('id'), 'backgroundColor': target.style('background-color')};
+                    target.style("background-color", "lightblue");
+                    var data = {
+                      id: target.data("id"),
+                      backgroundColor: target.style("background-color"),
+                    };
                     this.props.updateNodeColor(data);
                   },
                 },
                 {
-                  id: 'color-dark-blue',
-                  content: 'dark blue',
-                  tooltipText: 'dark blue',
-                  onClickFunction:(event) => {
+                  id: "color-dark-blue",
+                  content: "dark blue",
+                  tooltipText: "dark blue",
+                  onClickFunction: (event) => {
                     let target = event.target || event.cyTarget;
-                    target.style('background-color', '#0066cc');
-                    var data = {"id": target.data('id'), 'backgroundColor': target.style('background-color')};
+                    target.style("background-color", "#0066cc");
+                    var data = {
+                      id: target.data("id"),
+                      backgroundColor: target.style("background-color"),
+                    };
                     this.props.updateNodeColor(data);
                   },
                 },
               ],
             },
             {
-              id: 'color-green',
-              content: 'green',
-              tooltipText: 'green',
-              onClickFunction:  (event) => {
+              id: "color-green",
+              content: "green",
+              tooltipText: "green",
+              onClickFunction: (event) => {
                 let target = event.target || event.cyTarget;
-                target.style('background-color', 'green');
-                var data = {"id": target.data('id'), 'backgroundColor': target.style('background-color')};
+                target.style("background-color", "green");
+                var data = {
+                  id: target.data("id"),
+                  backgroundColor: target.style("background-color"),
+                };
                 this.props.updateNodeColor(data);
               },
             },
             {
-              id: 'color-red',
-              content: 'red',
-              tooltipText: 'red',
+              id: "color-red",
+              content: "red",
+              tooltipText: "red",
               onClickFunction: (event) => {
                 let target = event.target || event.cyTarget;
-                target.style('background-color', 'red');
-                var data = {"id": target.data('id'), 'backgroundColor': target.style('background-color')};
+                target.style("background-color", "red");
+                var data = {
+                  id: target.data("id"),
+                  backgroundColor: target.style("background-color"),
+                };
                 this.props.updateNodeColor(data);
               },
             },
-          ]
-        }
+          ],
+        },
       ],
       menuItemClasses: ["contextMenu"],
-      contextMenuClasses: ["contextMenu"], 
-      submenuIndicator: { src: require('./images/hamburger-menu-icon.svg').default, width: 14, height: 14 }
+      contextMenuClasses: ["contextMenu"],
+      submenuIndicator: {
+        src: require("./images/hamburger-menu-icon.svg").default,
+        width: 14,
+        height: 14,
+      },
     });
   }
 }
--- frontend/src/userInput.jsx
+++ frontend/src/userInput.jsx
@@ -1,86 +1,134 @@
-import React, { Component } from 'react';
-import { BlockPicker } from 'react-color';
-import { v4 as uuidv4 } from 'uuid';
+import React, { Component } from "react";
+import { BlockPicker } from "react-color";
+import { v4 as uuidv4 } from "uuid";
 
 class UserInput extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {id: uuidv4(), 
-                    label: '', 
-                    targetNode: '',
-                    backgroundColor:'#ff79c6'
-                    };
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-      this.handleColorChange = this.handleColorChange.bind(this);
-    }
-  
-    componentWillReceiveProps(nextProps) {
-      // check to prevent unnecessary render
-      if (nextProps.nodeToMod['id'] != this.state.id && nextProps.nodeToMod['id'] != '') {
-          this.setState({ id: nextProps.nodeToMod['id'], 
-          label: nextProps.nodeToMod['label'], 
-          backgroundColor:nextProps.nodeToMod['backgroundColor'] });
-      }
-    }
-    handleChange(event) {
-      const target = event.target;
-      const value = target.type === 'checkbox' ? target.checked : target.value;
-      const name = target.name;
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: uuidv4(),
+      label: "",
+      targetNode: "",
+      backgroundColor: "#ff79c6",
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleColorChange = this.handleColorChange.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    // check to prevent unnecessary render
+    if (
+      nextProps.nodeToMod["id"] != this.state.id &&
+      nextProps.nodeToMod["id"] != ""
+    ) {
       this.setState({
-        [name]: value
+        id: nextProps.nodeToMod["id"],
+        label: nextProps.nodeToMod["label"],
+        backgroundColor: nextProps.nodeToMod["backgroundColor"],
       });
-    }
-  
-    handleColorChange(color)  {
-      this.setState({
-        'backgroundColor': color.hex
-      });
-    }
-    
-    handleSubmit(event) {
-      if (this.state.id == "" || this.state.label == ""){
-        alert("Please fill in the Node Id and the Label to continue.");
-        return;
-      }
-      var data = {"id": this.state.id, "label": this.state.label, "target": this.state.targetNode, "backgroundColor": this.state.backgroundColor};
-      if (this.props.nodeToMod['id'].length >0){
-        this.props.updateElement(data);
-      }else{
-        this.props.saveNewElement(data);
-      }
-      //event.preventDefault();
-    }
-  
-    render() {
-      return (
-        <form onSubmit={this.handleSubmit}>
-          {this.props.nodeToMod['id'].length >0
-            ? <label>Modify node</label>
-            : <label>Add a new Node</label>
-          }
-          <br/><br/>
-          <label>
-            <input type="hidden" name="id" value={this.state.id} onChange={this.handleChange} />
-          </label>
-          <label>Node label:
-            <input type="text" name="label" value={this.state.label} onChange={this.handleChange} />
-          </label>
-          <label>Node color:
-            <BlockPicker triangle='hide' colors={['#ff79c6', '#8be9fd', '#50fa7b', '#ffb86c', '#bd93f9', '#ff5555', '#f1fa8c', '#ff8a65', '#ba68c8']} color={this.state.backgroundColor} onChange={this.handleColorChange} />
-          </label>
-          <label>
-            Select a target Node:
-            <select name="targetNode" value={this.state.target} onChange={this.handleChange} >
-              <option value='' >Select a target node</option>
-              {this.props.targetNodes.map(({ id, label }, index) => <option value={id}  key={id} >{label}</option>)}
-            </select>
-          </label>
-          <button type="submit"> Submit</button>
-        </form>
-      );
     }
   }
-  
-  export default UserInput;
+  handleChange(event) {
+    const target = event.target;
+    const value = target.type === "checkbox" ? target.checked : target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  handleColorChange(color) {
+    this.setState({
+      backgroundColor: color.hex,
+    });
+  }
+
+  handleSubmit(event) {
+    if (this.state.id == "" || this.state.label == "") {
+      alert("Please fill in the Node Id and the Label to continue.");
+      return;
+    }
+    var data = {
+      id: this.state.id,
+      label: this.state.label,
+      target: this.state.targetNode,
+      backgroundColor: this.state.backgroundColor,
+    };
+    if (this.props.nodeToMod["id"].length > 0) {
+      this.props.updateElement(data);
+    } else {
+      this.props.saveNewElement(data);
+    }
+    //event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        {this.props.nodeToMod["id"].length > 0 ? (
+          <label>Modify node</label>
+        ) : (
+          <label>Add a new Node</label>
+        )}
+        <br />
+        <br />
+        <label>
+          <input
+            type="hidden"
+            name="id"
+            value={this.state.id}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Node label:
+          <input
+            type="text"
+            name="label"
+            value={this.state.label}
+            onChange={this.handleChange}
+          />
+        </label>
+        <label>
+          Node color:
+          <BlockPicker
+            triangle="hide"
+            colors={[
+              "#ff79c6",
+              "#8be9fd",
+              "#50fa7b",
+              "#ffb86c",
+              "#bd93f9",
+              "#ff5555",
+              "#f1fa8c",
+              "#ff8a65",
+              "#ba68c8",
+            ]}
+            color={this.state.backgroundColor}
+            onChange={this.handleColorChange}
+          />
+        </label>
+        <label>
+          Select a target Node:
+          <select
+            name="targetNode"
+            value={this.state.target}
+            onChange={this.handleChange}
+          >
+            <option value="">Select a target node</option>
+            {this.props.targetNodes.map(({ id, label }, index) => (
+              <option value={id} key={id}>
+                {label}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button type="submit"> Submit</button>
+      </form>
+    );
+  }
+}
+
+export default UserInput;
