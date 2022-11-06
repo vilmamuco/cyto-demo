9458ec931bbf373194cbff905c57b5ede48e9f7e
Vilma Muço <muco.vilma@gmail.com>
2022-11-06 14:01:40 +0100

    blacken python code

--- backend/main.py
+++ backend/main.py
@@ -9,132 +9,173 @@ edges = mydb["edges"]
 
 app = Flask(__name__)
 
-@app.route('/nodes')
+
+@app.route("/nodes")
 def get_nodes():
-    return list(nodes.find({}, {"_id":0}))
+    return list(nodes.find({}, {"_id": 0}))
 
-@app.route('/edges')
+
+@app.route("/edges")
 def get_edges():
-    return list(edges.find({}, {"_id":0}))
+    return list(edges.find({}, {"_id": 0}))
 
-@app.route('/elements')
+
+@app.route("/elements")
 def get_elements():
     elements = []
-    elements += [node for node in nodes.find({}, {"_id":0})]
-    elements += [node for node in edges.find({}, {"_id":0})]
+    elements += [node for node in nodes.find({}, {"_id": 0})]
+    elements += [node for node in edges.find({}, {"_id": 0})]
     return jsonify(elements)
 
-@app.route('/elementIds')
+
+@app.route("/elementIds")
 def get_elementIds():
     elementIds = []
-    elementIds += [ node['data']['id'] for node in nodes.find({}, {"_id":0})]
+    elementIds += [node["data"]["id"] for node in nodes.find({}, {"_id": 0})]
     return jsonify(elementIds)
 
-@app.route('/getTargetNodes')
+
+@app.route("/getTargetNodes")
 def get_targetNodes():
     # send the target nodes for the select input for the user form
-    return jsonify([{'id': node['data']['id'], 'label': node['data']['label'] } for node in get_nodes()])
+    return jsonify(
+        [
+            {"id": node["data"]["id"], "label": node["data"]["label"]}
+            for node in get_nodes()
+        ]
+    )
 
-@app.route('/deleteNode', methods=['POST'], strict_slashes=False)
+
+@app.route("/deleteNode", methods=["POST"], strict_slashes=False)
 def delete_node():
-    #deletes the node and the edges connected to it 
-    id = request.json['id']
-    if (nodes.find() is not None ) and nodes.count_documents({'data.id': id}) > 0:
-        nodes.delete_one({'data.id':  id});
-        edges.delete_many({'$or': [{'data.target': id}, {'data.source': id}]});
+    # deletes the node and the edges connected to it
+    id = request.json["id"]
+    if (nodes.find() is not None) and nodes.count_documents({"data.id": id}) > 0:
+        nodes.delete_one({"data.id": id})
+        edges.delete_many({"$or": [{"data.target": id}, {"data.source": id}]})
     return get_elements()
 
-@app.route('/deleteEdge', methods=['POST'], strict_slashes=False)
+
+@app.route("/deleteEdge", methods=["POST"], strict_slashes=False)
 def delete_edge():
-    #deletes the node and the edges connected to it 
-    source = request.json['source']
-    target = request.json['target']
-    if (edges.find() is not None ) > 0:
-        edges.delete_many({'$and': [{'data.target': target}, {'data.source': source}]});
+    # deletes the node and the edges connected to it
+    source = request.json["source"]
+    target = request.json["target"]
+    if (edges.find() is not None) > 0:
+        edges.delete_many({"$and": [{"data.target": target}, {"data.source": source}]})
     return get_elements()
 
-@app.route('/updatePosition', methods=['POST'], strict_slashes=False)
+
+@app.route("/updatePosition", methods=["POST"], strict_slashes=False)
 def update_Position():
-    #saves the current position of the node in the database
-    id = request.json['data']['id']
-    x = request.json['position']['x']
-    y = request.json['position']['y']
+    # saves the current position of the node in the database
+    id = request.json["data"]["id"]
+    x = request.json["position"]["x"]
+    y = request.json["position"]["y"]
 
-    nodes.update_one({"data.id": id},
-            {"$set": {"position.x": x, "position.y": y}})
-    # maybe return just ok 
+    nodes.update_one({"data.id": id}, {"$set": {"position.x": x, "position.y": y}})
+    # maybe return just ok
     return get_elements()
 
-@app.route('/updateColor', methods=['POST'], strict_slashes=False)
+
+@app.route("/updateColor", methods=["POST"], strict_slashes=False)
 def update_Color():
-    #saves the current position of the node in the database
-    id = request.json['id']
-    color = request.json['backgroundColor']
+    # saves the current position of the node in the database
+    id = request.json["id"]
+    color = request.json["backgroundColor"]
 
-    nodes.update_one({"data.id": id},
-            {"$set": {"style.background-color": color}})
-    # maybe return just ok 
+    nodes.update_one({"data.id": id}, {"$set": {"style.background-color": color}})
+    # maybe return just ok
     return get_elements()
 
 
-@app.route('/saveNewElement', methods=['POST'], strict_slashes=False)
+@app.route("/saveNewElement", methods=["POST"], strict_slashes=False)
 def save_newElement():
-    #saves the new element in the corresponding tables in the database (nodes, edges)
+    # saves the new element in the corresponding tables in the database (nodes, edges)
     request_data = request.get_json()
-    id = request.json['id']
-    label = request.json['label']
-    target = None 
-    color = 'blue'
+    id = request.json["id"]
+    label = request.json["label"]
+    target = None
+    color = "blue"
     x = 0
     y = 0
 
-    if 'target' in request_data:
-        target = request.json['target']
-    if 'x' in request_data:
-        x = request.json['x']
-    if 'y' in request_data:
-        y = request.json['y']
-    if 'backgroundColor' in request_data:
-        color = request.json['backgroundColor']
+    if "target" in request_data:
+        target = request.json["target"]
+    if "x" in request_data:
+        x = request.json["x"]
+    if "y" in request_data:
+        y = request.json["y"]
+    if "backgroundColor" in request_data:
+        color = request.json["backgroundColor"]
 
-    if (nodes.find() is None ) or nodes.count_documents({'data.id': id}) == 0:
-        nodes.insert_one({"data": {'id': id, 'label':label}, 'position': {'x':x, 'y':y}, 'style': {'background-color': color}})
+    if (nodes.find() is None) or nodes.count_documents({"data.id": id}) == 0:
+        nodes.insert_one(
+            {
+                "data": {"id": id, "label": label},
+                "position": {"x": x, "y": y},
+                "style": {"background-color": color},
+            }
+        )
         if target:
-            edges.insert_one({'data':{'source':id, 'target': target, 'label':'test add'}})
+            edges.insert_one(
+                {"data": {"source": id, "target": target, "label": "test add"}}
+            )
     return get_elements()
 
-@app.route('/updateElement', methods=['POST'], strict_slashes=False)
+
+@app.route("/updateElement", methods=["POST"], strict_slashes=False)
 def update_element():
-    #updates the element in the corresponding tables in the database (nodes, edges)
+    # updates the element in the corresponding tables in the database (nodes, edges)
     request_data = request.get_json()
-    id = request.json['id']
-    label = request.json['label']
-    target = None 
-    color = 'blue'
+    id = request.json["id"]
+    label = request.json["label"]
+    target = None
+    color = "blue"
     x = 0
     y = 0
 
-    if 'target' in request_data:
-        target = request.json['target']
-    if 'x' in request_data:
-        x = request.json['x']
-    if 'y' in request_data:
-        y = request.json['y']
-    if 'backgroundColor' in request_data:
-        color = request.json['backgroundColor']
+    if "target" in request_data:
+        target = request.json["target"]
+    if "x" in request_data:
+        x = request.json["x"]
+    if "y" in request_data:
+        y = request.json["y"]
+    if "backgroundColor" in request_data:
+        color = request.json["backgroundColor"]
 
-    if (nodes.find() is not None ) and nodes.count_documents({'data.id': id}) > 0:
-        nodes.update_one({"data.id": id}, {"$set": {'data.label':label , "style.background-color": color}})
-        if target and edges.count_documents({'$and': [{'data.target': target}, {'data.source': id}]}) == 0:
-            edges.insert_one({'data':{'source':id, 'target': target, 'label':'test add'}});
+    if (nodes.find() is not None) and nodes.count_documents({"data.id": id}) > 0:
+        nodes.update_one(
+            {"data.id": id},
+            {"$set": {"data.label": label, "style.background-color": color}},
+        )
+        if (
+            target
+            and edges.count_documents(
+                {"$and": [{"data.target": target}, {"data.source": id}]}
+            )
+            == 0
+        ):
+            edges.insert_one(
+                {"data": {"source": id, "target": target, "label": "test add"}}
+            )
     return get_elements()
 
-@app.route('/addNewEdge', methods=['POST'], strict_slashes=False)
-def add_edge():
-    #updates the element in the corresponding tables in the database (nodes, edges)
-    source = request.json['source']
-    target = request.json['target']
 
-    if (edges.find() is None ) or ( edges.find() is not None and edges.count_documents({'$and': [{'data.target': target}, {'data.source': source}]}) == 0):
-        edges.insert_one({'data':{'source':source, 'target': target, 'label':'test add'}});
+@app.route("/addNewEdge", methods=["POST"], strict_slashes=False)
+def add_edge():
+    # updates the element in the corresponding tables in the database (nodes, edges)
+    source = request.json["source"]
+    target = request.json["target"]
+
+    if (edges.find() is None) or (
+        edges.find() is not None
+        and edges.count_documents(
+            {"$and": [{"data.target": target}, {"data.source": source}]}
+        )
+        == 0
+    ):
+        edges.insert_one(
+            {"data": {"source": source, "target": target, "label": "test add"}}
+        )
     return get_elements()
